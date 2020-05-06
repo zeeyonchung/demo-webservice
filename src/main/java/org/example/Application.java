a8dc8c951daa60a054253b027bada21af11f7846
Zeeyon <zeeyon.c@gmail.com>
2020-05-07 02:15:31 +0900

    start

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 프로젝트의 메인 클래스
+ * @SpringBootApplication   스프링부트 자동 설정, 스프링 bean 읽기/생성 자동 설정
+ *                          이 어노테이션이 있는 위치부터 설정을 읽어가므로 이 클래스는 항상 프로젝트 최상단에 있어야 한다.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        // 내장 WAS 실행 - 외부에 WAS를 두지 않고 스프링부트로 만들어진 Jar 파일로 실행
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/org/example/web/HelloControllerTest.java
@@ -0,0 +1,44 @@
+package org.example.web;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class) //Junit에 내장된 실행자 외에 다른 실행자를 실행시킨다. 스프링 부트 테스트와 JUnit 사이의 연결자 역할.
+@WebMvcTest(controllers = HelloController.class) //Spring MVC에 집중하는 테스트 어노테이션. @Controller, @ControllerAdvice 사용 가능.
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc; //웹API 테스트 시 사용. HTTP GET, POST 등에 대한 API 테스트.
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+}
