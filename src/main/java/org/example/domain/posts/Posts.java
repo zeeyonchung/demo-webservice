d09179222a92fcf32167b7f124af84407fbc5128
Zeeyon <zeeyon.c@gmail.com>
2020-05-10 01:47:45 +0900

    JPA Auditing으로 엔티티 생성/수정 시간 저장

--- src/main/java/org/example/domain/posts/Posts.java
+++ src/main/java/org/example/domain/posts/Posts.java
@@ -3,13 +3,14 @@ package org.example.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.domain.BaseTimeEntity;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
