a4b75379db6d489fd2d6a08742246d68368b7d3b
Zeeyon <zeeyon.c@gmail.com>
2020-05-10 00:58:33 +0900

    게시글 Posts 기본 CRUD

---
+++ src/main/java/org/example/service/PostsService.java
@@ -0,0 +1,39 @@
+package org.example.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.domain.posts.Posts;
+import org.example.domain.posts.PostsRepository;
+import org.example.web.dto.PostsResponseDto;
+import org.example.web.dto.PostsSaveRequestDto;
+import org.example.web.dto.PostsUpdateRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
