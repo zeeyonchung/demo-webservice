5fff8de293166e83737366a4e59bd261053c1841
Zeeyon <zeeyon.c@gmail.com>
2020-05-17 20:18:45 +0900

    기본 CRUD 기능, mustache 사용한 페이지 구현

--- src/main/java/org/example/service/PostsService.java
+++ src/main/java/org/example/service/PostsService.java
@@ -3,13 +3,18 @@ package org.example.service;
 import lombok.RequiredArgsConstructor;
 import org.example.domain.posts.Posts;
 import org.example.domain.posts.PostsRepository;
+import org.example.web.dto.PostsListResponseDto;
 import org.example.web.dto.PostsResponseDto;
 import org.example.web.dto.PostsSaveRequestDto;
 import org.example.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class PostsService {
 
@@ -30,10 +35,24 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
